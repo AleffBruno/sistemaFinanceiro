9fe869945181ee7df5a67d95e2b700b577449af4
Aleff <aleffbruno@gmail.com>
2018-04-24 16:44:15 -0300

    Initializing balance controller

---
+++ resources/views/admin/home/index.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'view/admin/home/index/TITLE')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <p>You are logged in!</p>
+@stop
